b07f20cd948ec3fc669bef695b647d9f34eac539
Dan Currier <dcurrier585@gmail.com>
2022-12-21 13:19:53 -0500

    added constructors forprogram

---
+++ constructors/manager.js
@@ -0,0 +1,15 @@
+class Manager extends Employee {
+    constructor(officeNumber) {
+        this.officeNumber = officeNumber;
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+
+    getRole() {
+        return 'Manager';
+    }
+}
+
+module.exports = Manager;
